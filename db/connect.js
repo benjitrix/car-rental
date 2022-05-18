bf77f0701318454de1ab0087082ca7fa6e0add94
benjamin <benchude@gmail.com>
2022-05-18 10:12:02 +0100

    removed options from connectDB

--- db/connect.js
+++ db/connect.js
@@ -1,13 +1,7 @@
 const mongoose = require('mongoose')
 
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false
-}
-
 const connectDB = (url) => {
-  return mongoose.connect(url, options)
+  return mongoose.connect(url)
 }
 
 module.exports = connectDB
