601db92aa6e6a3bc753dace5645006b1be7c38c3
benjamin <benchude@gmail.com>
2022-05-18 10:00:17 +0100

    updated for heroku production

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(errorHandlerMiddleware)
 // production 
 if (process.env.NODE.ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")))
-  app.get("/*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   })
 }
--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose')
 
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+}
+
 const connectDB = (url) => {
-  return mongoose.connect(url)
+  return mongoose.connect(url, options)
 }
 
 module.exports = connectDB
